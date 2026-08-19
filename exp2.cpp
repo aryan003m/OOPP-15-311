4c2d87997186bac49c171e4e87781fa1fbc9a5e6
Aryan Mavi <aryan009m@gmail.com>
2026-08-19 10:26:56 +0530

    swap by value, reference and address

--- exp2.cpp
+++ exp2.cpp
@@ -1,41 +1,62 @@
-// #include <bits/stdc++.h>
-// using namespace std;
-// void swapByValue(int a, int b){
-//     int temp = a;
-//     a = b;
-//     b = temp;
-   
-// }
-// void swapByRefrence(int &a, int &b){
-//      int temp = a;
-//     a = b;
-//     b = temp;
-//      cout<<"Swap by ref:"<<"a:"<<a<<"b:"<<b<<endl;
-    
-// }
-// void swapByAddress(int*a, int*b){
-//     int temp = *a;
-//     *a = *b;
-//     *b = temp;
-//      cout<<"Swap by address:"<<"a:"<<a<<"b:"<<b<<endl;
+#include <bits/stdc++.h>
+using namespace std;
 
-// }
-// int main(){
-//     int a,b;
-//     cout<<"enter numbers:";
-//     cin>>a>>b;
-//     cout<<"before swaping:"<<"a:"<<a<<"b:"<<b<<endl;
-//     cout<<"after swaping:"<<swapByValue(a,b)<<endl;
+void swapByValue(int a, int b) {
+    int temp = a;
+    a = b;
+    b = temp;
 
-//     cout<<"before swaping:"<<"a:"<<a<<"b:"<<b<<endl;
-//     cout<<"after swaping"<<swapByRefrence(a,b)<<endl;
+    cout << "Inside function (Pass by Value): ";
+    cout << "a = " << a << ", b = " << b << endl;
+}
 
-//     cout<<"before swaping:"<<"a:"<<a<<"b:"<<b<<endl;
-//     cout<<"after swaping"<<swapByAddress(&a,&b)<<endl;
+void swapByReference(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+    cout << "Inside function (Pass by Reference): ";
+    cout << "a = " << a << ", b = " << b << endl;
+}
 
-//     return 0;
+void swapByAddress(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+    cout << "Inside function (Pass by Address): ";
+    cout << "a = " << *a << ", b = " << *b << endl;
+}
 
-// }
+int main() {
+
+    int a, b;
+    cout << "Enter numbers: ";
+    cin >> a >> b;
+    cout << "Before swap by value: ";
+    cout << "a = " << a << ", b = " << b << endl;
+
+    swapByValue(a, b);
+
+    cout << "After swap by value: ";
+    cout << "a = " << a << ", b = " << b << endl;
+
+    cout << "\nBefore swap by reference: ";
+    cout << "a = " << a << ", b = " << b << endl;
+
+    swapByReference(a, b);
+
+    cout << "After swap by reference: ";
+    cout << "a = " << a << ", b = " << b << endl;
+
+    cout << "Before swap by address: ";
+    cout << "a = " << a << ", b = " << b << endl;
+
+    swapByAddress(&a, &b);
+
+    cout << "After swap by address: ";
+    cout << "a = " << a << ", b = " << b << endl;
+
+    return 0;
+}
 
 // #include <bits/stdc++.h>
 // using namespace std;
@@ -104,52 +125,52 @@
 //     return 0;
 // }
 
-#include <bits/stdc++.h>
-using namespace std;
-class Point{
-    private:
-    int x;
-    int y;
-    public:
-    // Point(): x{0}, y{0} {
-    //     cout<<"default"<<endl;
-    // }
-    // Point(int a, int b): x{a}, y{b} {
-    //     cout<<"Parametrized"<<endl;
-    // }
-//----------------------- Two IN One--------------------------------
-    Point(int p = 0, int q = 0): x{p}, y{q} {}
+// #include <bits/stdc++.h>
+// using namespace std;
+// class Point{
+//     private:
+//     int x;
+//     int y;
+//     public:
+//     // Point(): x{0}, y{0} {
+//     //     cout<<"default"<<endl;
+//     // }
+//     // Point(int a, int b): x{a}, y{b} {
+//     //     cout<<"Parametrized"<<endl;
+//     // }
+// //----------------------- Two IN One--------------------------------
+//     Point(int p = 0, int q = 0): x{p}, y{q} {}
 
-    // Point add(Point p){
-    //     Point r;
-    //     r.x = x + p.x;
-    //     r.y = y + p.y;
-    //     return r;       
-    // }
-    //sorter version
+//     // Point add(Point p){
+//     //     Point r;
+//     //     r.x = x + p.x;
+//     //     r.y = y + p.y;
+//     //     return r;       
+//     // }
+//     //sorter version
     
-    // Point add(Point q){
-    //     return Point(x+q.x,y+q.y);
-    // }
-    // For two object arguments
-    Point add(Point p, Point q){
-        return Point( p.x + q.x, p.y + q.y);
-    }
+//     // Point add(Point q){
+//     //     return Point(x+q.x,y+q.y);
+//     // }
+//     // For two object arguments
+//     Point add(Point p, Point q){
+//         return Point( p.x + q.x, p.y + q.y);
+//     }
       
-    void show(){
-        cout<<x<<","<<y;
-    }
-};
-int main(){
+//     void show(){
+//         cout<<x<<","<<y;
+//     }
+// };
+// int main(){
     
-    Point p(5,10),q(20,50);
-    p.show();
-   cout<<endl;
-    q.show();
-    cout<<endl;
-    // Point r = p.add(q);
-    Point r = r.add(p,q);
+//     Point p(5,10),q(20,50);
+//     p.show();
+//    cout<<endl;
+//     q.show();
+//     cout<<endl;
+//     // Point r = p.add(q);
+//     Point r = r.add(p,q);
 
-    r.show();
-    return 0;
-}
+//     r.show();
+//     return 0;
+// }
